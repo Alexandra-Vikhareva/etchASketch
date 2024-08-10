14ce9af95e03139814a9262a1d6f8a72fa3a11f3
Sasha <st087955@student.spbu.ru>
2024-08-10 20:59:40 +0300

    add function that colors the cells

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-draw = document.querySelector('.draw');
-console.log(draw);
+function drawGrid (n = 16) {
 
-for (let i = 0; i < 16; i++) {
-    const box = document.createElement('div');
-    box.className = 'box';
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div');
-        cell.className = 'cell';
-        box.appendChild(cell);
+    draw = document.querySelector('.draw');
+
+    for (let i = 0; i < n; i++) {
+        const box = document.createElement('div');
+        box.className = 'box';
+        for (let j = 0; j < n; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            box.appendChild(cell);
+        };
+        draw.appendChild(box);
     };
-    draw.appendChild(box);
+    
+    function sketch () {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => {
+                cell.classList.add('black');
+            });
+        });
+    };
+
+    sketch();
 }
+
+drawGrid();
